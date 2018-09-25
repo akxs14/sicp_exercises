f8c8e373dce22b44de36fbd8a19047dd4a7921d1
Angelos <angelos.kapsimanis@gmail.com>
2018-09-26 00:46:16 +0200

    Added 1.11

--- chapter_1/exercise_1_11.rkt
+++ chapter_1/exercise_1_11.rkt
@@ -1,12 +1,14 @@
 #lang racket
 
-#| 	Exercise 1.11: A function ff is defined by the rule that 
-	f(n)=nf(n)=n if n<3n<3 and f(n)=f(n−1)+2f(n−2)+3f(n−3)f(n)=f(n−1)+2f(n−2)+3f(n−3) 
-	if n≥3n≥3. Write a procedure that computes ff by means of a recursive process. 
+#| 	Exercise 1.11: A function ff is defined by the rule that
+	   f(n)=nf(n)=n if n<3n<3
+    and
+       f(n)=f(n−1)+2f(n−2)+3f(n−3)f(n) if n≥3n≥3
+	Write a procedure that computes ff by means of a recursive process.
 	Write a procedure that computes ff by means of an iterative process.
 |#
 
-# Iterative version
+; Recursive version
 
 (define (f-recur n)
   (if (< n 3)
@@ -21,3 +23,23 @@
 (f-recur 3) ; 3
 (f-recur 4) ; 11
 (f-recur 5) ; 25
+
+
+; Iterative version
+
+(define (f-iter a b c count)
+    (if (= count 3)
+        (+ a (* 2 b) (* 3 c))
+        (f-iter (+ a (* 2 b) (* 3 c)) a b (- count 1))))
+
+(define (f n)
+    (if (< n 3)
+        n
+        (f-iter 2 1 0 n)))
+
+
+(f 1) ; 1
+(f 2) ; 2
+(f 3) ; 3
+(f 4) ; 11
+(f 5) ; 25
